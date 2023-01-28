6016af52c40e9f354e9483015e5abc97ee5c8e0c
Lumino-2-0 <123825334+Lumino-2-0@users.noreply.github.com>
2023-01-28 23:42:26 +0100

    Add files via upload

---
+++ Luminoware/Effect User/anim_NCS.cs
@@ -0,0 +1,141 @@
+﻿/*
+    
+
+
+    _                    _            _       _           _           _  _  _           _           _          _  _  _  _                                                   _  _  _  _                 _              _           _          _  _  _  _           _  _  _  _         _           _    
+   (_)                  (_)          (_)     (_) _     _ (_)         (_)(_)(_)         (_) _       (_)       _(_)(_)(_)(_)_                                                (_)(_)(_)(_) _            _(_)_           (_) _       (_)       _(_)(_)(_)(_)_       _(_)(_)(_)(_)_      (_) _     _ (_)   
+   (_)                  (_)          (_)     (_)(_)   (_)(_)            (_)            (_)(_)_     (_)      (_)          (_)                                               (_)         (_)         _(_) (_)_         (_)(_)_     (_)      (_)          (_)     (_)          (_)     (_)(_)   (_)(_)   
+   (_)                  (_)          (_)     (_) (_)_(_) (_)            (_)            (_)  (_)_   (_)      (_)          (_)                                               (_) _  _  _ (_)       _(_)     (_)_       (_)  (_)_   (_)      (_)_  _  _  _        (_)          (_)     (_) (_)_(_) (_)   
+   (_)                  (_)          (_)     (_)   (_)   (_)            (_)            (_)    (_)_ (_)      (_)          (_)                                               (_)(_)(_)(_)         (_) _  _  _ (_)      (_)    (_)_ (_)        (_)(_)(_)(_)_      (_)          (_)     (_)   (_)   (_)   
+   (_)                  (_)          (_)     (_)         (_)            (_)            (_)      (_)(_)      (_)          (_)                                               (_)   (_) _          (_)(_)(_)(_)(_)      (_)      (_)(_)       _           (_)     (_)          (_)     (_)         (_)   
+   (_) _  _  _  _       (_)_  _  _  _(_)     (_)         (_)          _ (_) _          (_)         (_)      (_)_  _  _  _(_)                                               (_)      (_) _       (_)         (_)      (_)         (_)      (_)_  _  _  _(_)     (_)_  _  _  _(_)     (_)         (_)   
+   (_)(_)(_)(_)(_)        (_)(_)(_)(_)       (_)         (_)         (_)(_)(_)         (_)         (_)        (_)(_)(_)(_)                                                 (_)         (_)      (_)         (_)      (_)         (_)        (_)(_)(_)(_)         (_)(_)(_)(_)       (_)         (_)   
+     __ __             __     
+    /\ \\ \          /'__`\   
+    \ \ \\ \        /\ \/\ \  
+     \ \ \\ \_      \ \ \ \ \ 
+      \ \__ ,__\ __  \ \ \_\ \
+       \/_/\_\_//\_\  \ \____/
+          \/_/  \/_/   \/___/ 
+
+
+
+
+                                                                                                                                                                                                                                                                                             
+*/
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace animANDNCS
+{
+    internal class Anim_NCS
+    {
+        
+        public void Del_NCS_Ani()
+        {
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(1000);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(2000);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(1000);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+        }
+
+        public void Del_NCS_Ani2()
+        {
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(500);
+            SendKeys.Send("{NUMLOCK}");
+            Console.Beep(125, 200);
+            SendKeys.Send("{NUMLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{CAPSLOCK}");
+            Console.Beep(500, 200);
+            SendKeys.Send("{CAPSLOCK}");
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{SCROLLLOCK}");
+            Console.Beep(1000, 200);
+            SendKeys.Send("{SCROLLLOCK}");
+        }
+
+
+    }
+}
