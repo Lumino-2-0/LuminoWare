0fc8841ba6b39a748fdd573463b47440ef08259e
Lumino-2-0 <123825334+Lumino-2-0@users.noreply.github.com>
2023-01-29 10:35:08 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("luminoware")]
+[assembly: AssemblyDescription("RANSOMWARE")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lumine")]
+[assembly: AssemblyProduct("Lumino_Ransom")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("Lumino")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7ab0dd04-43e0-4d89-be59-60a30b766467")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.5.4.0")]
+[assembly: AssemblyFileVersion("8.1.4.666")]
+[assembly: NeutralResourcesLanguage("en")]
